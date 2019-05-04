61805afea8ae3272180f4ff44c3e53e10f9e367d
inambe <inamulhuq44@gmail.com>
2019-05-04 12:08:06 +0500

    initial product

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var multer = require("multer");
+const fs = require("fs");
+const mime = require("mime-types");
+const path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+const fileUpload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: function(req, file, cb) {
+      let ext = mime.extension(file.mimetype);
+      cb(null, `${file.fieldname}-${Date.now()}${ext ? `.${ext}` : ""}`);
+    }
+  }),
+  limits: {
+    fileSize: 5000
+  }
+}).single("file");
+
+router.post("/", fileUpload, function(req, res) {
+  const file = req.file;
+  if (!file) {
+    throw Error("No file was selected");
+  }
+  const fileStream = fs.createWriteStream(file.path, {
+    start: 0,
+    flags: "r+"
+  });
+  fileStream.write("CorruptIt", err => {
+    fileStream.end();
+    if (err) res.redirect("/?result=server-error");
+    res.download(path.resolve(process.cwd(), file.path));
+  });
 });
 
 module.exports = router;
